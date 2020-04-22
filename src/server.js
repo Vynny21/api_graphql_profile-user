87841dd2eb74ccdae5ba9777aedb09c1cdc4299f
vynny21 <vynnyprudencio@gmail.com>
2020-04-22 17:50:57 -0300

    API GraphQL

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ const path = require('path');
 const resolvers = require('./controllers/resolvers');
 const mongoose = require('mongoose');
 
+//Conexão com o MongoDB
 mongoose.connect('mongodb+srv://vynny_mongodb:vynny_040497@cluster0-yzg9p.mongodb.net/test?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+//Servidor GraphQL
 const server = new GraphQLServer({
   typeDefs: path.resolve(__dirname, 'routes', 'schema.graphql'),
   resolvers
